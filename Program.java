7ed23d8c9a33c2399d1adf94da1a19241afafad1
janzzle <janzzleworld@gmail.com>
2021-08-19 20:30:34 +0900

    메인에서 생성한 객체를 메소드 내에서 사용하는 방법 공부

--- Program.java
+++ Program.java
@@ -5,12 +5,11 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-
-		Exam[] exams = new Exam[3];
 		ExamList list = new ExamList();
-		list.exams = new Exam[3];
+		list.exams = new Exam[3];  // before : Exam[] exams = new Exam[3];
 		list.current = 0;
-		
+		list.exam = new Exam();
+
 		int menu;
 		boolean keepLoop = true;
 		
@@ -39,8 +38,8 @@ public class Program {
 		System.out.println("성적 출력");
 		int size = list.current;
 		for(int i=0; i<size; i++) {
-			Exam exam = list.exams[i];
-			int kor = exam.kor;	//임시 변수 사용 
+			Exam exam = list.exams[i]; // (5) 여기는 메인 메서드에서 list.exams로 객체 생성,
+			int kor = exam.kor;		   // (6) 파라미터로 같이 들어왔기 때문에 객체 생성 없이 사용 가능 
 			int eng = exam.eng;
 			int math = exam.math;
 			
@@ -57,7 +56,7 @@ public class Program {
 		}
 	}
 
-	private static void inputList(ExamList list) { // 지역변수 current를 넘겨받음 
+	private static void inputList(ExamList list) {  
 		Scanner scan = new Scanner(System.in);
 	    System.out.println("성적 입력");
 	   	int kor, eng, math;
@@ -88,13 +87,17 @@ public class Program {
 	          System.out.println("성적 범위(0~100)를 벗어났습니다");
 	        } 
 	      } while(math < 0 || 100 < math);
-      		Exam exam = new Exam();
-	      	exam.kor = kor;
+      		
+//      	Exam exam = list.exam;  // (3) 방법이라면 list 데이터로 exam을 만들고 list.exam에 객체를 만들어주면 
+      								// (4) 파라미터 list에 같이 받아올 수 있으므로 좌측과 같이 사용 가
+
+      		Exam exam = new Exam(); // (1) 여기서 Exam 객체 미 생성시 밑으로 에러 발생, 
+	      	exam.kor = kor;			// (2) 메인 메서드에 생성해도 불가(파라미터로 들어오지 않기 때문)
 	      	exam.eng = eng;
 	      	exam.math = math;
 	      	
 	      	list.exams[list.current] = exam;
-	      	list.current++;			// current 사본에 +1 하므로 원 current는 그대로 0
+	      	list.current++;			
 	}
 			
 	static int inputMenu() {
@@ -108,6 +111,7 @@ public class Program {
 }
 class ExamList{
 	Exam[] exams;
+	Exam exam;
 	int current;
 	
 }
